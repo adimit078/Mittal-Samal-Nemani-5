50133b70a171b20fd0303791b7fdcc8eb5b32e7e
ssamal1 <sanatsamal14@gmail.com>
2021-12-16 15:13:40 -0800

    JS Image Rollover

---
+++ style.js
@@ -0,0 +1,17 @@
+$(document).ready(
+	function(){		
+		"use strict";
+		$("figure").mouseenter(
+			function(){
+				$("figcaption", this).stop().animate({"top":"0%",}, 200, 'swing');
+			}
+		);
+		$("figure").mouseleave(
+			function(){
+				$("figcaption", this).stop().animate({"top":"100%",}, 400, 'swing');
+			}
+		);
+	}
+);
+
+https://codepen.io/muoivy/pen/qYwooe
